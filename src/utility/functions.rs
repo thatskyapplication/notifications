31824b186913b51ddc9a1cbba4923a99aa5a5db2
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-10-18 10:02:00 +0100

    refactor(functions): Simplify return

--- src/utility/functions.rs
+++ src/utility/functions.rs
@@ -11,6 +11,5 @@ pub fn last_day_of_month(now: DateTime<Tz>) -> u32 {
         NaiveDate::from_ymd_opt(year, month + 1, 1).unwrap()
     };
 
-    let last_day_of_current_month = first_day_of_next_month - Duration::days(1);
-    last_day_of_current_month.day()
+    (first_day_of_next_month - Duration::days(1)).day()
 }
